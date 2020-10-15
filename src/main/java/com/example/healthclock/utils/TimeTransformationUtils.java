ee5f4f4a3903cf719767596739f291fab2dfa87f
wei_tung <1371690483@qq.com>
2020-10-15 22:21:47 +0800

    时间戳转换的工具类封装

---
+++ src/main/java/com/example/healthclock/utils/TimeTransformationUtils.java
@@ -0,0 +1,79 @@
+package com.example.healthclock.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * @Auther: 杨伟栋
+ * @Date: 2020/10/15 21:33
+ * @Description: 1371690483@qq.com  时间和时间戳转换类
+ */
+public class TimeTransformationUtils {
+    private static SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+    private static SimpleDateFormat df1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    /*
+     * @Author 杨伟栋
+     * @Description 将特定日期转换成时间戳
+     * @Date 2020/10/15 9:36 下午
+     * @Param date 特定的日期(相当于当日的零点)
+     * @return
+     **/
+    public static long DateToTimestmp(String date) {
+
+        try {
+            return df.parse(date).getTime()/1000;
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return 0;
+    }
+
+    /*
+     * @Author 杨伟栋
+     * @Description 特定时间点转换时间戳
+     * @Date 2020/10/15 9:57 下午
+     * @Param date日期 time 时间  (2:30:00)
+     * @return
+     **/
+    public static long SpecTimeToTimestmp(String date,String time) {
+        String complete_time = date+" "+time;
+        try {
+            return df1.parse(complete_time).getTime()/1000;
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    /*
+     * @Author 杨伟栋
+     * @Description 获取当前时间(不带时分秒)
+     * @Date 2020/10/15 9:48 下午
+     * @Param
+     * @return
+     **/
+    public static String SysCurrTime() {
+        return df.format(new Date());
+    }
+
+    /*
+     * @Author 杨伟栋
+     * @Description 获取当天的后一天的 时间
+     * @Date 2020/10/15 10:15 下午
+     * @Param
+     * @return
+     **/
+    public static String TomorrowDate() {
+        Calendar c = Calendar.getInstance();
+        c.setTime(new Date());
+        c.add(Calendar.DAY_OF_MONTH,1);
+        Date tomorrow = c.getTime();
+        return df.format(tomorrow);
+
+    }
+
+}
--- src/test/java/com/example/healthclock/HealthclockApplicationTests.java
+++ src/test/java/com/example/healthclock/HealthclockApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.healthclock;
 
+import com.example.healthclock.utils.TimeTransformationUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +10,27 @@ class HealthclockApplicationTests {
     @Test
     void contextLoads() {
     }
+
+    @Test
+    void test() {
+        long l = TimeTransformationUtils.DateToTimestmp("2020-10-15");
+        System.out.println(l);
+
+    }
+    @Test
+    void test1() {
+        System.out.println(TimeTransformationUtils.TomorrowDate());
+        System.out.println(System.currentTimeMillis() / 1000);
+        System.out.println(TimeTransformationUtils.SysCurrTime());
+
+    }
+
+    @Test
+    void test2() {
+        long l = TimeTransformationUtils.SpecTimeToTimestmp(TimeTransformationUtils.SysCurrTime(),"2:30:00");
+        System.out.println(l);
+
+    }
+
 
 }
